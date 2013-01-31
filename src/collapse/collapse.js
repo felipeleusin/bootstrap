65b0b07c9fbf0076651d04438c05d47ca0ecc43d
Gleb Godonoga <gleb.godonoga@gmail.com>
2013-01-31 12:32:36 +0200

    Revert "Height fix when is not visible during binding"
    This reverts commit 7543d35cc502165b0faf03c0d84280d36a0f514a.

--- src/collapse/collapse.js
+++ src/collapse/collapse.js
@@ -25,11 +25,7 @@ angular.module('ui.bootstrap.collapse',['ui.bootstrap.transition'])
       var initialHeightVal = element[0].scrollHeight;
       var heightProperlyInitialized = false;
 
-      var whenHidden = function (){ return element[0].scrollHeight !== 0; };
-      var whenVisible = function (){ return initialHeightVal !== element[0].scrollHeight; };
-      var heightWatcher = initialHeightVal === 0 ? whenHidden : whenVisible;
-
-      scope.$watch(heightWatcher, function (value) {
+      scope.$watch(function (){ return initialHeightVal != element[0].scrollHeight; }, function (value) {
         //The listener is called when angular bindings are being resolved
         //When we have a change of height after binding we are setting again the correct height if the group is opened
         if (value && !heightProperlyInitialized) {
